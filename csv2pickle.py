4e27fd85487898209969cf88a1d589aaa3859892
siahuat0727 <tansiahuat@gmail.com>
2022-03-08 07:55:05 +0800

    First commit

---
+++ csv2pickle.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+
+def transform_csv2pickle(path, usecols, dtypes):
+    train = pd.read_csv(
+        path,
+        usecols=usecols,
+        dtype=dtypes
+    )
+    train.to_pickle('train.pkl')
+
+
+def main():
+    path = '../kaggle/train.csv'
+
+    basecols = ['row_id', 'time_id', 'investment_id', 'target']
+    features = [f'f_{i}' for i in range(300)]
+
+    dtypes = {
+        'row_id': 'str',
+        'time_id': 'uint16',
+        'investment_id': 'uint16',
+        'target': 'float32',
+    }
+    for col in features:
+        dtypes[col] = 'float32'
+    transform_csv2pickle(path, basecols+features, dtypes)
+
+
+
+def read():
+    train = pd.read_pickle('train.pkl')
+    print(train)
+
+# main()
+read()
